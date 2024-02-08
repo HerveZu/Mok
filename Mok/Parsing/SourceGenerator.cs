13c24d132697ced545c61eef7694fa362969cd56
Herve.Zucchinetti <herve.zucchinetti@bobst.com>
2024-02-08 17:42:01 +0100

    Add source generator

---
+++ Mok/Parsing/SourceGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Mok.Parsing;
+
+[Generator]
+internal sealed class SourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+    }
+}

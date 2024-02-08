85276f76e1cba62d0d4cfba6fccb7925579295df
Herve.Zucchinetti <herve.zucchinetti@bobst.com>
2024-02-08 17:41:34 +0100

    Add InterceptsLocationAttribute

---
+++ Mok/CompilerServices/InterceptsLocationAttribute.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+[UsedImplicitly]
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public sealed class InterceptsLocationAttribute(
+    [UsedImplicitly] string filePath,
+    [UsedImplicitly] int line,
+    [UsedImplicitly] int character
+) : Attribute
+{
+}

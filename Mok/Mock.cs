db641d53366bf52f246c5b87ef6a8e1d0ced4c75
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2024-02-09 11:14:44 +0100

    Throw exception when the mocked service access has not been intercepted

--- Mok/Mock.cs
+++ Mok/Mock.cs
@@ -6,7 +6,10 @@ namespace Mok;
 public sealed class Mock<T> : IMock<T>
     where T : class
 {
-    public T Object { get; } = null!;
+    public T Object => throw new InvalidOperationException(
+        $"This mock has not been generated." +
+        $" Make sure to directly reference a mock from a field decorated with'{nameof(UseMockAttribute)}'"
+    );
 
     public ISetup<TResult> Setup<TResult>(Func<T, TResult> setup)
     {
